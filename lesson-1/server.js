61d60ba862dcc8d67776ed33bf7808e10593259f
Suvonov2000 <suvonov200@gmail.com>
2025-08-25 14:56:27 +0900

    post

--- lesson-1/server.js
+++ lesson-1/server.js
@@ -1,9 +1,20 @@
 import express from "express";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Express is successfully installed!");
+// app.get("/", (req, res) => {
+//   res.status(200).send("Express is successfully installed!");
+// });
+
+app.post("/", (req, res) => {
+  const { name, surname } = req.body;
+  console.log(req.body);
+
+  res.send(`Heloo:${name} ${surname}`);
 });
 
 const PORT = process.env.PORT || 2000;
