cf5dc4beea2eeea7015cc839cce8009eb807408c
Suvonov2000 <suvonov200@gmail.com>
2025-09-01 12:52:26 +0900

    routers

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -1,18 +1,18 @@
-// import express from "express";
+import express from "express";
 
-// const app = express();
+const app = express();
 
-// app.get("/user/:id", (req, res) => {
-//   const userID = req.params.id;
-//   res.send(`USer ID is ${userID}`);
-// });
+app.get("/user/:id", (req, res) => {
+  const userID = req.params.id;
+  res.send(`USer ID is ${userID}`);
+});
 
-// app.get("/search", (req, res) => {
-//   const { term, page } = req.query;
-//   res.send(`Searching for "${term} on page${page}`);
-// });
-// const PORT = process.env.PORT || 1000;
+app.get("/search", (req, res) => {
+  const { term, page } = req.query;
+  res.send(`Searching for "${term} on page${page}`);
+});
+const PORT = process.env.PORT || 1000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server is runing on port http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is runing on port http://localhost:${PORT}`);
+});
