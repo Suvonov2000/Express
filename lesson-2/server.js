29fe4823afc9d2bc49a09be2a5fd93ead56ad2d4
Suvonov2000 <suvonov200@gmail.com>
2025-09-03 15:27:31 +0900

    routers

--- lesson-2/server.js
+++ lesson-2/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/api/users/", user_router);
 app.use("/api/posts/", post_router);
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT},http://localhost:${PORT}`);
 });
