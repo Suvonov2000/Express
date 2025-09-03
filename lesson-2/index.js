5ef079f3921a14e3025a529e8da092182759f785
Suvonov2000 <suvonov200@gmail.com>
2025-09-03 15:30:09 +0900

    routers

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -11,7 +11,7 @@ app.get("/search", (req, res) => {
   const { term, page } = req.query;
   res.send(`Searching for "${term} on page${page}`);
 });
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, () => {
   console.log(`Server is runing on port http://localhost:${PORT}`);
