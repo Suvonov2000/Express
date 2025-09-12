75a38896bd58803ac26b93000033599d5488a28e
Suvonov2000 <suvonov200@gmail.com>
2025-09-12 23:05:30 +0900

    error handling

--- lesson-3/server.js
+++ lesson-3/server.js
@@ -4,27 +4,43 @@ config();
 
 const app = express();
 
-let count = 0;
+// let count = 0;
 
-const blogMiddleware = (req, res, next) => {
-  if (count <= 3) {
-    count++;
-    return next();
-  }
+// const blogMiddleware = (req, res, next) => {
+//   if (count <= 3) {
+//     count++;
+//     return next();
+//   }
 
-  res.status(400).send("Limit hit are not available now");
-};
+//   res.status(400).send("Limit hit are not available now");
+// };
 
-app.get("/blog", blogMiddleware, (req, res) => {
-  res.send(`Blogs-> ${count}`);
+// app.get("/blog", blogMiddleware, (req, res) => {
+//   res.send(`Blogs-> ${count}`);
+// });
+
+// let count_news = 0;
+// app.get("/news", (req, res) => {
+//   count_news++;
+//   res.send(`News -> ${count_news}`);
+// });
+
+app.get("/hello", (req, res) => {
+  res.send("Hello world!");
 });
 
-let count_news = 0;
-app.get("/news", (req, res) => {
-  count_news++;
-  res.send(`News -> ${count_news}`);
+app.get("/error", (req, res) => {
+  const err = new Error("Intentional error.");
+  err.status = 500;
+  next(err);
 });
 
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
 const PORT = process.env || 2020;
 console.log(process.env.PORT);
 
